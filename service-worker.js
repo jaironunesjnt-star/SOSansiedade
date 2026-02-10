7d3f5b8efebd79dad3beb2aad78c98a3c0b3c08a
Inovacoes <jaironunesjnt@gmail.com>
2026-02-11 00:06:17 +0100

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = 'sos-ansiedade-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './translations.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE = "sos-ansiedade-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./translations.js",
+  "./manifest.json"
 ];
 
-// Instala o service worker e salva arquivos no cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Intercepta requisições e serve do cache quando possível
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
